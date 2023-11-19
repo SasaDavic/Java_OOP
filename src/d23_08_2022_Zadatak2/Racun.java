898a506f96b42c9964e783d8ebd1e150e2c06da9
Sasa Davic <sasadavic.nis@gmail.com>
2023-11-19 20:51:37 +0100

    domaci 23 08 2022

--- src/d23_08_2022_Zadatak2/Racun.java
+++ src/d23_08_2022_Zadatak2/Racun.java
@@ -1,5 +1,45 @@
 package d23_08_2022_Zadatak2;
 
 public class Racun {
-
+	
+	private String brRacuna;
+	private String imeKlijenta;
+	private double trenutnoStanje;
+		
+	public Racun(String brRacuna, String imeKlijenta, double trenutnoStanje) {
+		this.brRacuna = brRacuna;
+		this.imeKlijenta = imeKlijenta;
+		this.trenutnoStanje = trenutnoStanje;
+	}
+	public String getBrRacuna() {
+		return brRacuna;
+	}
+	public void setBrRacuna(String brRacuna) {
+		this.brRacuna = brRacuna;
+	}
+	public String getImeKlijenta() {
+		return imeKlijenta;
+	}
+	public void setImeKlijenta(String imeKlijenta) {
+		this.imeKlijenta = imeKlijenta;
+	}
+	public double getTrenutnoStanje() {
+		return trenutnoStanje;
+	}
+	
+	public boolean promenaStanja(double uplata) {
+		if (this.trenutnoStanje + uplata < 0) {
+			System.out.println("Nemate dovoljno sredstava na racunu!");
+			return false;
+		} else {
+			this.trenutnoStanje += uplata;
+			return true;
+		}
+	}
+	
+	public void stampaj() {
+		System.out.println(this.imeKlijenta + " - " + this.brRacuna);
+		System.out.println("Stanje na racunu je " + this.trenutnoStanje + " rsd.");
+	}
+	
 }
--- src/d23_08_2022_Zadatak2/Transakcija.java
+++ src/d23_08_2022_Zadatak2/Transakcija.java
@@ -1,5 +1,59 @@
 package d23_08_2022_Zadatak2;
 
 public class Transakcija {
+	private String id;
+	private Racun odlivSredstava;
+	private Racun prilivSredstava;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Racun getOdlivSredstava() {
+		return odlivSredstava;
+	}
+	public void setOdlivSredstava(Racun odlivSredstava) {
+		this.odlivSredstava = odlivSredstava;
+	}
+	public Racun getPrilivSredstava() {
+		return prilivSredstava;
+	}
+	public void setPrilivSredstava(Racun prilivSredstava) {
+		this.prilivSredstava = prilivSredstava;
+	}
+	public Transakcija(String id, Racun odlivSredstava, Racun prilivSredstava) {
+		this.id = id;
+		this.odlivSredstava = odlivSredstava;
+		this.prilivSredstava = prilivSredstava;
+	}
+	public Transakcija() {
+	
+	}
+	
+	private double provizija(double prenos) {
+		if (prenos < 4500) {
+			return 45;
+		} else {
+			return prenos * 0.01;
+		}
+	}
+
+	public void prenosSred(double prenos) {
+		double x = -1 * ( prenos + provizija(prenos) );
+		if(this.odlivSredstava.promenaStanja(x) == true) {
+			this.odlivSredstava.promenaStanja(x);
+		}	
+	}
+	
+	public void stampaj( ) {
+		System.out.println("id transakcije: " + this.id);
+		System.out.print("Racun sa: ");
+		this.odlivSredstava.stampaj();
+		System.out.print("Racun na: ");
+		this.prilivSredstava.stampaj();
+	}
 
 }
